19f73e83686a3e6d93269f9b218347a016677b5f
jcasson <jcasson@gmail.com>
2013-11-09 11:42:30 +0000

    Congratulations screen has animated balloons and a title. Needs balloons hiding before they're animated, title cenetered and more balloons.

--- src/activities/Congratulations.java
+++ src/activities/Congratulations.java
@@ -4,6 +4,7 @@ import meerkatchallenge.activities.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
@@ -14,18 +15,26 @@ public class Congratulations extends Activity {
 		setContentView(R.layout.activity_congratulations);
 		
 		ImageView balloon1 = (ImageView) findViewById(R.id.balloon_1);
-		Animation bounce = AnimationUtils.loadAnimation(this, R.anim.balloon_float);
-		balloon1.startAnimation(bounce);
+		balloon1.startAnimation(getFlyUpBounce());
 		
 		ImageView balloon2 = (ImageView) findViewById(R.id.balloon_2);
-		Animation bounce2 = AnimationUtils.loadAnimation(this, R.anim.balloon_float);
-		bounce2.setStartOffset(750);
-		balloon2.startAnimation(bounce2);
+		new DelayedAnimation(balloon2, getFlyUpBounce(), 1000).execute();
 		
 		ImageView balloon3 = (ImageView) findViewById(R.id.balloon_3);
-		Animation bounce3 = AnimationUtils.loadAnimation(this, R.anim.balloon_float);
-		bounce3.setStartOffset(1250);
-		balloon3.startAnimation(bounce3);
+		new DelayedAnimation(balloon3, getFlyUpBounce(), 1500).execute();
+	}
+	
+	/**
+	 * Returns an animationset to fly ballons up and bounce them
+	 * @return The animationset
+	 */
+	private AnimationSet getFlyUpBounce() {
+		Animation bounce = AnimationUtils.loadAnimation(this, R.anim.balloon_bounce);
+		Animation flyUp = AnimationUtils.loadAnimation(this, R.anim.float_up);
+		AnimationSet flyUpBounce = new AnimationSet(false);
+		flyUpBounce.addAnimation(flyUp);
+		flyUpBounce.addAnimation(bounce);
+		return flyUpBounce;
 	}
 
 }
